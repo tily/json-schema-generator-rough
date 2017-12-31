e38a10c77aae27f0eab0a787e15f579ed768efb2
tily <tidnlyam@gmail.com>
2017-12-31 10:22:50 +0900

    `bundle gem json-schema-generator-rough`

---
+++ lib/json/schema/generator/rough.rb
@@ -0,0 +1,11 @@
+require "json/schema/generator/rough/version"
+
+module Json
+  module Schema
+    module Generator
+      module Rough
+        # Your code goes here...
+      end
+    end
+  end
+end
---
+++ lib/json/schema/generator/rough/version.rb
@@ -0,0 +1,9 @@
+module Json
+  module Schema
+    module Generator
+      module Rough
+        VERSION = "0.1.0"
+      end
+    end
+  end
+end
